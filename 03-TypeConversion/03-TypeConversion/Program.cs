386bd8a118a579b377b7eba71c79ef3210ff7bc8
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 15:35:10 +0930

    03-TypeConversion

--- 03-TypeConversion/03-TypeConversion/Program.cs
+++ 03-TypeConversion/03-TypeConversion/Program.cs
@@ -1,24 +1,36 @@
-﻿namespace HelloWorld
+﻿namespace TypeConversion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            byte b = 5;
-            int i = b;
-            Console.WriteLine(i);
+            try
+            {
+                byte b = 5;
+                int i = b;
+                Console.WriteLine(i);
 
-            int i2 = 6;
-            byte b2 = (byte)i2;
-            Console.WriteLine(b2);
+                int i2 = 6;
+                byte b2 = (byte)i2;
+                Console.WriteLine(b2);
 
-            int i3 = 555;
-            byte b3 = (byte)i3;
-            Console.WriteLine(b3);
+                int i3 = 555;
+                byte b3 = (byte)i3;
+                Console.WriteLine(b3);
 
-            var numStr = "34567";
-            int num = Convert.ToInt32(numStr);
-            Console.WriteLine(num);
+                var numStr = "34567";
+                var num = Convert.ToInt32(numStr);
+                Console.WriteLine(num);
+
+                string boolStr = "false";
+                bool flag = Convert.ToBoolean(boolStr);
+                Console.WriteLine(flag);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Casting Error!");
+            }
+            
         }
     }
 }
