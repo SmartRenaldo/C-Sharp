db1fdd43b244878b2092004dc9783d63813fc603
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 16:52:47 +0930

    06-Array

--- 06-Array/06-Array/Program.cs
+++ 06-Array/06-Array/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            var numArr = new int[3];
+            var numArr = new int[4];
+            numArr[0] = 1;
+
+            Console.WriteLine(numArr[0]);
+            Console.WriteLine(numArr[1]);
+            Console.WriteLine(numArr[2]);
         }
     }
 }
