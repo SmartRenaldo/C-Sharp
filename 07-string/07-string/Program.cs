894046c4475d79b7f0fb2fac2b4f64bf8605f525
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 17:11:52 +0930

    07-string

--- 07-string/07-string/Program.cs
+++ 07-string/07-string/Program.cs
@@ -9,6 +9,11 @@
             var strArr = new string[] { "aaa", "bbb", "ccc" };
             string list = string.Join(",", strArr);
             Console.WriteLine(list);
+
+            var text = @"Hi John
+c:\folder1\folder2
+c:\folder2\folder2";
+            Console.WriteLine(text);
         }
     }
 }
