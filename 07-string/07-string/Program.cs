3cab365e6703b0fb075684227b972d939616136f
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 17:10:20 +0930

    07-string

---
+++ 07-string/07-string/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Zoe";
+            Console.WriteLine(firstName[0]);
+            var strArr = new string[] { "aaa", "bbb", "ccc" };
+            string list = string.Join(",", strArr);
+            Console.WriteLine(list);
+        }
+    }
+}
