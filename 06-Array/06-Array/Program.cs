1674388e79796fcf523e94b04ae5ffd54a382ced
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 16:54:12 +0930

    06-Array

--- 06-Array/06-Array/Program.cs
+++ 06-Array/06-Array/Program.cs
@@ -6,10 +6,17 @@
         {
             var numArr = new int[4];
             numArr[0] = 1;
-
             Console.WriteLine(numArr[0]);
             Console.WriteLine(numArr[1]);
             Console.WriteLine(numArr[2]);
+
+            var flags = new bool[5];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+            Console.WriteLine(flags[3]);
+            Console.WriteLine(flags[4]);
         }
     }
 }
