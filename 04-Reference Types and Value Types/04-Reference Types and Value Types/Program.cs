2f47b54bc2c6dfcfe7dd3e1c9ec75029b059900d
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 16:03:24 +0930

    05-ClassDemo

--- 04-Reference Types and Value Types/04-Reference Types and Value Types/Program.cs
+++ 04-Reference Types and Value Types/04-Reference Types and Value Types/Program.cs
@@ -8,6 +8,11 @@
             var b = a;
             b++;
             Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            var array1 = new int[5] { 1, 2, 3, 4, 5 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
         }
     }
 }
---
+++ 05-ClassDemo/05-ClassDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpFundamentals
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void bark()
+        {
+            Console.WriteLine("Barking...");
+            Console.WriteLine(Name);
+            Console.WriteLine(Description);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cinnamon = new Dog();
+            cinnamon.Name = "Cinnamon";
+            cinnamon.Description = "A golden hair dog";
+            cinnamon.bark();
+        }
+    }
+}
