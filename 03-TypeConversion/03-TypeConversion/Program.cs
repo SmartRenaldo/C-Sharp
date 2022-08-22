bd13fabc6060860e3d9a5b9747e3b0aa1fb52f0f
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 14:53:53 +0930

    add 03-TypeConversion

---
+++ 03-TypeConversion/03-TypeConversion/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5;
+            int i = b;
+            Console.WriteLine(i);
+
+            int i2 = 5;
+            byte b2 = (byte)i;
+            Console.WriteLine(b2);
+        }
+    }
+}
