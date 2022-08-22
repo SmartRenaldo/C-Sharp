76254ecdad9f005bb84da517d1228df547b0f2ac
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-22 14:36:01 +0930

    add 02-Variables and Constants

---
+++ 02-Variables and Constants/02-Variables and Constants/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 5;
+            Console.WriteLine(number);
+
+            const int conNum = 7;
+            Console.WriteLine(conNum);
+
+            string name = "Tara";
+            Console.WriteLine(name);
+
+            var num2 = 10;
+            Console.WriteLine(num2);
+
+            var price = 10.99f;
+            Console.WriteLine(price);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
