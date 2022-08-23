0d96553fe7b06e7317f34ccef3d38ce39effb7b1
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 11:27:34 +0930

    11 Random

---
+++ 12-Array/12-Array/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4 };
+            Console.WriteLine(Array.IndexOf(numbers, 3));
+            Console.WriteLine();
+
+            Array.Clear(numbers, 0, 2);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            int[] newNumbers = new int[3];
+            Array.Copy(numbers, 1, newNumbers, 0, 3);
+            Console.WriteLine();
+
+            foreach (var newNumber in newNumbers)
+            {
+                Console.WriteLine(newNumber);
+            }
+
+
+            var matrix = new int[3, 5]
+            {
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 },
+                { 1, 2, 3, 4, 5 }
+            };
+
+            /*foreach (int i in matrix)
+            {
+                foreach (int j in i)
+                {
+                    Console.WriteLine(j); 
+                }
+            }*/
+        }
+    }
+}
