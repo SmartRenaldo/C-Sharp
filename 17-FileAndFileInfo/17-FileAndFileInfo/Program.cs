67d5310cc4ebe4a1929354e3466c1068ce6c3a20
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 15:56:09 +0930

    17 file and fileInfo

--- 17-FileAndFileInfo/17-FileAndFileInfo/Program.cs
+++ 17-FileAndFileInfo/17-FileAndFileInfo/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            var targetPath = "C:\\Projects\\C#\\17-FileAndFileInfo\\copyTest.txt";
+            var srcPath = "C:\\Projects\\C#\\17-FileAndFileInfo\\test.txt";
+            File.Copy(srcPath, targetPath, true);
 
+            if (File.Exists(targetPath))
+            {
+                Console.WriteLine("Target File exists!");
+                var srcContent = File.ReadAllText(srcPath);
+                Console.WriteLine(srcContent);
+            }
+
+            File.Delete(targetPath);
+
+            var fileInfo = new FileInfo(srcPath);
+            fileInfo.CopyTo(targetPath, true);
         }
     }
 }
