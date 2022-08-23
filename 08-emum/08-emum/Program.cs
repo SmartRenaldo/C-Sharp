872f2d201ef59cd5b2af9bdd91b72762cdaa7435
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 09:40:31 +0930

    08 enum

---
+++ 08-emum/08-emum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpFundamentals
+{
+    public enum Shipping
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var express = Shipping.Express;
+            Console.WriteLine(express);
+            Console.WriteLine((int)express);
+
+            Console.WriteLine((Shipping)2);
+
+            Console.WriteLine(express.ToString());
+            var shippingExpress = (Shipping)Enum.Parse(typeof(Shipping), express.ToString());
+            Console.WriteLine(shippingExpress);
+        }
+    }
+}
