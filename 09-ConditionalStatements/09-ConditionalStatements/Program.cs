8947320123f0836fc8e8b0061aff9a49b4e42c36
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 09:53:12 +0930

    09-Conditional Statements

---
+++ 09-ConditionalStatements/09-ConditionalStatements/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 11;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Morning");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("Afternoon");
+            }
+            else
+            {
+                Console.WriteLine("Evening");
+            }
+        }
+    }
+}
