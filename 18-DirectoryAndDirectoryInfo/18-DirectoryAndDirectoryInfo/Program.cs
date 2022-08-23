9b612ae2bfd98036df666503efcffcc835b220b5
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 16:14:18 +0930

    18 directory and directory info

---
+++ 18-DirectoryAndDirectoryInfo/18-DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("C:\\Projects\\C#\\18-DirectoryAndDirectoryInfo\\testDir");
+            var files = Directory.GetFiles("C:\\Projects\\C#\\18-DirectoryAndDirectoryInfo\\18-DirectoryAndDirectoryInfo\\obj");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories("C:\\Projects\\C#\\18-DirectoryAndDirectoryInfo", "*.*", SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+        }
+    }
+}
