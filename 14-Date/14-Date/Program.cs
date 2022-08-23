4a2b1e9fa25b4e347c7581477db9aac84526a51c
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 13:26:25 +0930

    14-Date

---
+++ 14-Date/14-Date/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            numbers.Add(9);
+            numbers.AddRange(new int[2] { -4, 0 });
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(numbers.IndexOf(-5));
+            Console.WriteLine(numbers.IndexOf(5));
+            Console.WriteLine("Count: " + numbers.Count);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 9)
+                    numbers.Remove(numbers[i]);
+            }
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        }
+    }
+}
