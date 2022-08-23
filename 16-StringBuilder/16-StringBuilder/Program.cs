111ed2e93506600cb99aaefeeb14c22b196bb7ba
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 15:00:38 +0930

    16 string builder

---
+++ 16-StringBuilder/16-StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Method");
+            builder.Replace('-', '+');
+            builder.Remove(0, 5);
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
