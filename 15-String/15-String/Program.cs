920b74d09abe7753939df11ef536678ab10b3de6
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-23 14:38:39 +0930

    string

--- 15-String/15-String/Program.cs
+++ 15-String/15-String/Program.cs
@@ -4,36 +4,20 @@
     {
         static void Main(string[] args)
         {
-            var dateTime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            var fullName = "  Christina Liu ";
+            Console.WriteLine(fullName.Trim());
+            Console.WriteLine(fullName.Trim().ToLower());
+            Console.WriteLine(fullName.Trim().ToUpper());
 
-            Console.WriteLine(now);
-            Console.WriteLine(now.Hour);
-            Console.WriteLine(now.Minute);
-            Console.WriteLine(today);
+            var names = fullName.Trim().Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
 
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
-            Console.WriteLine(tomorrow.ToLongDateString());
-            Console.WriteLine(tomorrow.ToShortDateString());
-            Console.WriteLine(tomorrow.ToLongTimeString());
-            Console.WriteLine(tomorrow.ToShortTimeString());
-            Console.WriteLine(tomorrow.ToString("yyyy MM dd HH mm ss"));
+            var newFullName = fullName.Replace("Christina", "Christine");
+            Console.WriteLine(newFullName.Trim());
 
-            var timeSpan = new TimeSpan(1, 2, 3);
-            var timeSpan1 = new TimeSpan(1, 0, 0);
-            var timeSpan2 = TimeSpan.FromHours(1);
-            var start = DateTime.Now;
-            var end = DateTime.Now.AddMinutes(2);
-            var duration = end - start;
-            Console.WriteLine(duration);
-
-            Console.WriteLine(timeSpan);
-            Console.WriteLine("Total days: " + timeSpan.TotalDays);
-            Console.WriteLine("Days: " + timeSpan.Days);
-
-            Console.WriteLine("Add example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            float price = 19.99f;
+            Console.WriteLine(price.ToString("C"));
         }
     }
 }
