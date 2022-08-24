6c2a16981b77ee31d7b8b7105848f1f347dfcb6d
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-24 09:35:59 +0930

    19 Debug

---
+++ 19-Debug/19-Debug/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.WriteLine("Hello");
+        }
+    }
+}
